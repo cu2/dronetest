7b233798cafcebaa7cbaad9858592a2a6ce12160
Andras Nemeth <andras.nemeth@instructure.com>
2020-02-04 16:47:40 +0100

    unit tests added

---
+++ test_app.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+class AppError(Exception):
+    pass
+
+
+def inc(x):
+    return x + 1
+
+
+def raise_app_error():
+    raise AppError()
+
+
+def test_inc():
+    assert inc(3) == 4
+
+
+def test_raise_app_error():
+    with pytest.raises(AppError):
+        raise_app_error()
